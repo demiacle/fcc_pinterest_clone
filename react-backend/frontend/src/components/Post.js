765d78791bda425815db3fde5492cf22a4153706
demiacle <demiacle@gmail.com>
2018-01-19 14:27:56 -0800

    Added post styles

--- react-backend/frontend/src/components/Post.js
+++ react-backend/frontend/src/components/Post.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import './Post.css';
 
 class Post extends Component {
-  render(){
-      var i = this.props.postData
-      return <li className="grid-item">
-          <a href={i.link}>
-            <img src={i.link} alt={i.caption} />
-            <div>
-              <div className="userPortrait">
-                <img src="https://twitter.com/PUBGpartners/profile_image?size=normal" alt="user portrait" />
-              </div>
-              likes:<span>{i.thumbsUp}</span></div>
-            <p className="caption">{i.caption}</p>
-          </a>
-        </li>
+  render() {
+    var i = this.props.postData
+    return <li className="grid-item">
+      <a href={i.link}>
+        <img src={i.link} alt={i.caption} />
+      </a>
+      <div className="postHeader">
+        <a href={ "/postsBy/" + i.user }>
+        <div className="userPortrait">
+          <img src={"https://twitter.com/" + i.user + "/profile_image?size=normal"} alt="user portrait" />
+        </div>
+        <p className="userName">{i.user}</p>
+        </a>
+        <a href={"/vote/" + i._id }>
+        <span className="likes"><img src="three-arrow.png" />{i.thumbsUp}</span>
+        </a>
+      </div>
+      <p className="caption">{i.caption}</p>
+    </li>
   }
 }
 
