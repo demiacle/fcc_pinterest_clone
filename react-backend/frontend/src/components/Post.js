8dbb209d726e770a90c6b2a3ae553d414c524a25
demiacle <demiacle@gmail.com>
2018-01-20 15:20:22 -0800

    Added broken link img

--- react-backend/frontend/src/components/Post.js
+++ react-backend/frontend/src/components/Post.js
@@ -5,18 +5,24 @@ class Post extends Component {
   //TODO fetch vote
 
   //TODO fetch posts by user
+  setDefaultPortrait(e){
+    e.target.src = "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png"
+  }
+  setDefaultImage(e){
+    e.target.src = "broken-link.png"
+  }
 
   render() {
     var i = this.props.postData
     return <li className="grid-item">
       <a href={i.link}>
-        <img src={i.link} alt={i.caption} />
+        <img src={i.link} alt={i.caption} onError={this.setDefaultImage}/>
       </a>
       <div className="postHeader">
         <a href={"/posts-by/" + i.user}>
           <div className="portrait-name-container">
-            <div className="userPortrait">
-              <img src={i.user.profileUrl} alt="user portrait" />
+            <div className="user-portrait">
+              <img src={i.user.profileUrl} alt="user portrait" onError={this.setDefaultPortrait} />
             </div>
             <p className="userName">{i.user.userName}</p>
           </div>
